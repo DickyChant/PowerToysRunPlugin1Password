6674aaf396e38538715f0e01d76ed68311246d25
Sitian Qian <stqian@pku.edu.cn>
2026-03-07 00:53:35 +0100

    Fix: target net9.0, restore ManagedCommon for Theme, update SDK

--- Main.cs
+++ Main.cs
@@ -2,6 +2,7 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more info_rmation.
 
+using ManagedCommon;
 using Wox.Plugin.Logger;
 using Microsoft.PowerToys.Settings.UI.Library;
 using OnePassword;
